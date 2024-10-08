41a284da9400a787471dda4ea2b148042b222e09
PineappleCreator <eitan.mosko@icloud.com>
2024-10-08 18:08:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let total=document.getElementById( 'total')
+
+let amount=0
+
+function take(){
+amount=amount-1
+total.innerHTML=amount
+}
+
+function add(){
+    amount=amount+1
+total.innerHTML=amount
+}
+
+
+//+חיבור
+//-מינוס
+// /חילוק
+//*כפל
+//**חזק
+//--להוריד אחד
+//++לעלוצ אחד
+//%שארית חלוקה
